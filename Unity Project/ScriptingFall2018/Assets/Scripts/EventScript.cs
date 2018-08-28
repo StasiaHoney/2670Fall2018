24626500894bdc2bf935dd5d8e40599a083b0201
Unknown <kosukeinuzuka@gmail.com>
2018-08-28 11:15:40 -0600

    Set up Jetbrains Rider and played around with Unity events

---
+++ Unity Project/ScriptingFall2018/Assets/Scripts/EventScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+public class EventScript : MonoBehaviour
+{
+
+	public UnityEvent MyEvent;
+
+	private void OnMouseDown()
+	{
+		MyEvent.Invoke();
+	}
+}
