21a9b0a7c3d29424721df8814bf392ecc5c78e31
Unknown <kosukeinuzuka@gmail.com>
2018-09-12 14:22:00 -0600

    made an airplane thingy and attempted a double jump

--- Unity Project/ScriptingFall2018/Assets/Scripts/MovePattern.cs
+++ Unity Project/ScriptingFall2018/Assets/Scripts/MovePattern.cs
@@ -3,24 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 [CreateAssetMenu]
 public class MovePattern : ScriptableObject {
-
-    public FloatData jumpSpeed;
+    
     public FloatData gravity;
     public FloatData MoveX, MoveY, MoveZ;
     public FloatData Rotx, Roty, Rotyz;
 	
-    private Vector3 moveDirection;
+    protected Vector3 moveDirection;
     private Vector3 rotDirection;
 
-    public void Invoke(CharacterController controller, Transform transform)
+    public virtual void Invoke(CharacterController controller, Transform transform)
     {
-        if (controller.isGrounded) {
-            moveDirection.Set(MoveX.Value, MoveY.Value, MoveZ.Value);
-            rotDirection.Set(Rotx.Value, Roty.Value, Rotyz.Value);
-            transform.Rotate(rotDirection);
-            moveDirection = transform.TransformDirection(moveDirection);
+        if (controller.isGrounded)
+        {
+            Move(transform);
         }
+
+        Move(controller);
+    }
+
+    protected void Move(CharacterController controller)
+    {
         moveDirection.y -= gravity.Value * Time.deltaTime;
         controller.Move(moveDirection * Time.deltaTime);
+    }
+
+    protected void Move(Transform transform)
+    {
+        moveDirection.Set(MoveX.Value, MoveY.Value, MoveZ.Value);
+        rotDirection.Set(Rotx.Value, Roty.Value, Rotyz.Value);
+        transform.Rotate(rotDirection);
+        moveDirection = transform.TransformDirection(moveDirection);
     }
 }
