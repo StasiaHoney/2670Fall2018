545f65057c9e4cf8347f4c7cd16c41fcbaca59a4
Unknown <kosukeinuzuka@gmail.com>
2018-09-17 14:18:57 -0600

    started creating an AI scout and has it return to base

---
+++ Unity Project/ScriptingFall2018/Assets/Scripts/AIAgent.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class AIAgent : MonoBehaviour
+{
+
+	private NavMeshAgent agent;
+	public Transform Destination;
+	public Transform PostPoint;
+	private Transform finalDestiation;
+	
+	
+	void Start ()
+	{
+		agent = GetComponent<NavMeshAgent>();
+		finalDestiation = transform;
+	}
+
+	private void OnTriggerEnter(Collider obj)
+	{
+		if(obj.transform == Destination)
+			finalDestiation = Destination;
+	}
+
+	private void OnTriggerExit(Collider obj)
+	{
+		finalDestiation = PostPoint;
+	}
+
+	private void Update()
+	{
+		agent.destination = finalDestiation.position;
+	}
+}
