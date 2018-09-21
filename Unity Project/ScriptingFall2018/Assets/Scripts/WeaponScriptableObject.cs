5ca8d44a2f4c40e1a4a41553abe853c90f8ede5c
Unknown <kosukeinuzuka@gmail.com>
2018-09-20 23:19:59 -0600

    created a weapon scriptable object and weapon switch script

---
+++ Unity Project/ScriptingFall2018/Assets/Scripts/WeaponScriptableObject.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu (fileName = "NewWeapon", menuName = "Weapon")]
+public class WeaponScriptableObject : ScriptableObject
+{
+
+	public GameObject weapon;
+
+	public virtual GameObject Weapon
+	{
+		get { return weapon; }
+	}
+
+}
---
+++ Unity Project/ScriptingFall2018/Assets/Scripts/WeaponSwitch.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters;
+using UnityEngine;
+using UnityEngine.Experimental.PlayerLoop;
+
+public class WeaponSwitch : MonoBehaviour
+{
+    public int currentWeapon;
+    public Transform [] weapons;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            changeWeapon(1);
+        }
+    }
+
+    public void changeWeapon(int num)
+    {
+        for (int i = 0; i < weapons.Length; i++)
+        {
+            if(i == num)
+                
+                weapons[i].gameObject.SetActive(true);
+            else
+            {
+                weapons[i].gameObject.SetActive(false);
+            }
+        }
+    }
+}
