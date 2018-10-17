358c0191b130af9c87e1ff3799d3b1212532ad70
Unknown <kosukeinuzuka@gmail.com>
2018-10-17 06:39:02 -0600

    added a menu, made skybox materials, need camera drag script

---
+++ Unity Project/ScriptingFall2018/Assets/Scripts/TiltGame/ScrollDrag.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollDrag : MonoBehaviour {
+
+	private Vector3 offsetPosition;
+	private Vector3 newPosition;
+	private Camera cam;
+	public bool CanDrag;
+    
+	private void Start()
+	{
+		cam = Camera.main;
+	}
+
+	private IEnumerator OnMouseDown()
+	{
+		offsetPosition = transform.position - cam.ScreenToWorldPoint(Input.mousePosition);
+		yield return new WaitForFixedUpdate();
+		CanDrag = true;
+		while (CanDrag)
+		{
+			yield return new WaitForFixedUpdate();
+			newPosition = cam.ScreenToWorldPoint(Input.mousePosition) + offsetPosition;
+			transform.position = newPosition;
+		}
+	}
+
+	private void OnMouseUp()
+	{
+		CanDrag = false;
+	}
+}
