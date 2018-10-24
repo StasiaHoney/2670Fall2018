df4081e6126b59231d1dfd90cb7bc549965f3fef
Unknown <kosukeinuzuka@gmail.com>
2018-10-24 14:31:15 -0600

    Working on hypercasual game lecture

---
+++ Unity Project/ScriptingFall2018/Assets/Scripts/DebugObject.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "DebugObject", menuName = "Objects/Debug Object")]
+public class DebugObject : ScriptableObject {
+
+    public void Call(string s)
+    {
+        Debug.Log(s);
+    }
+}
--- Unity Project/ScriptingFall2018/Assets/Scripts/WordDemo/SnapOn.cs
+++ Unity Project/ScriptingFall2018/Assets/Scripts/WordDemo/SnapOn.cs
@@ -5,25 +5,36 @@ using UnityEngine;
 public class SnapOn : MonoBehaviour
 {
 
-    private Vector3 position;
+    private Transform parent;
     public FloatData Speed;
-
+    public FloatData HoldTime;
+    private bool canRun = true;
+    
     private void OnTriggerEnter(Collider other)
     {
-        position = other.transform.position;
+        parent = other.transform;
     }
 
     public void Call()
     {
         StartCoroutine(MoveTo());
+        StartCoroutine(Stop());
+    }
+
+    private IEnumerator Stop()
+    {
+        yield return new WaitForSeconds(HoldTime.Value);
+        canRun = false;
     }
 
     IEnumerator MoveTo()
     {
-        while (true)
+        while (canRun)
         {
             yield return new WaitForFixedUpdate();
-            transform.position = Vector3.Lerp(transform.position, position, 1.0f);
+            transform.position = Vector3.Lerp(transform.position, parent.position, Speed.Value);
         }
+
+        transform.parent = parent;
     }
 }
