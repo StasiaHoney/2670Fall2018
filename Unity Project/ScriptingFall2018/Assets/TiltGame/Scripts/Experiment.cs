fba47ac70f9bd482ce6f483bbe9f7e40478c09b0
Unknown <kosukeinuzuka@gmail.com>
2018-10-25 23:39:43 -0600

    attempted a better rotate script, it failed miserably

---
+++ Unity Project/ScriptingFall2018/Assets/TiltGame/Scripts/Experiment.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Experiment : MonoBehaviour
+{
+float rotSpeed = 20;
+
+void OnMouseDrag(){
+    float rotX = Input.GetAxis("Mouse X") * rotSpeed * Mathf.Rad2Deg;
+    float rotY = Input.GetAxis("Mouse Y") * rotSpeed * Mathf.Rad2Deg;
+
+    transform.RotateAround(Vector3.up, -rotX);
+   transform.RotateAround(Vector3.right, rotY);
+}
+}
+
---
+++ Unity Project/ScriptingFall2018/Assets/TiltGame/Scripts/RotateObject.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObject : MonoBehaviour
+{
+	public bool CanRotate;
+	private Vector3 newPosition;
+	private Vector3 offsetPosition;
+	private Camera cam;
+
+
+	private void OnMouseDown()
+	{
+		CanRotate = true;
+		while (CanRotate)
+		{
+			
+		}
+	}
+
+
+	private void OnMouseUp()
+	{
+		CanRotate = false;
+	}
+}
