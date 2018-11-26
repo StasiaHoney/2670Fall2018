023d495276f617165932bd2d239cab5b74eb4fcf
Unknown <kosukeinuzuka@gmail.com>
2018-11-26 14:23:52 -0700

    worked on store a bit and fixed a bit of a bug with level 2

--- Unity Project/ScriptingFall2018/Assets/Scripts/WordDemo/Store/PurchasableObject.cs
+++ Unity Project/ScriptingFall2018/Assets/Scripts/WordDemo/Store/PurchasableObject.cs
@@ -5,17 +5,26 @@ public class PurchasableObject : ScriptableObject
 {
 
 	public Object Item;
+	public int UsageCount = 3;
 	public int Value;
 	public bool Perpetual;
 	public bool Instantiatable;
 
 	public void CreateItem()
 	{
-		if (Instantiatable && Item is GameObject)
+		for (int i = 0; i < UsageCount; i++)
 		{
-			Instantiate(Item);
-			
+			if (Instantiatable && Item is GameObject)
+            		{
+            			Instantiate(Item);
+            			
+            		}
 		}
+		
 	}
 
+	public void Used()
+	{
+		UsageCount--;
+	}
 }
