e1725447ebad584c17c57ead46e73529ca234126
Unknown <kosukeinuzuka@gmail.com>
2018-12-05 14:15:25 -0700

    worked on store

---
+++ Unity Project/ScriptingFall2018/Assets/TiltGame/Scripts/IntToText.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IntToText : MonoBehaviour
+{
+
+	public IntData Integer;
+	public Text Label;
+
+	private void Start()
+	{
+		Label.text = Integer.Value.ToString();
+	}
+}
---
+++ Unity Project/ScriptingFall2018/Assets/TiltGame/Scripts/StoreV2.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(menuName = "Store/StoreV2")]
+public class StoreV2 : ScriptableObject
+{
+
+	public Object Item;
+	public List<Object> Purchased;
+	public int ItemValue;
+	public IntData Cash;
+
+	public void Purchase()
+	{
+		if (Cash.Value >= ItemValue)
+		{
+			Purchased.Add(Item);
+			Cash.Value -= ItemValue;
+		}
+	}
+
+	public void AddObjectToGame()
+	{
+		if (Purchased.Contains(Item))
+		{
+			Instantiate(Purchased[0]);
+			Purchased.RemoveAt(0);
+		}
+	}
+}
